7329fcc886d95bdcebd8cdd7239403d28ad87a70
code-creater <rino2000@yandex.ru>
2017-07-09 21:06:21 +0300

    STT has done

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -4,6 +4,10 @@ import Type
 import Data.List
 import Reduce
 
+freshType :: [Var] -> Var
+freshType arr = head $ filter (`notElem` arr) a
+            where a = [V $ x : show i | i <- [1..] :: [Int], x <- ['A'..'Z']]
+
 composeSubs :: Substitution -> Substitution -> Substitution
 composeSubs a b = map (\(TS x t) -> TS x $ subsT a t) b ++ a
 
@@ -14,35 +18,36 @@ subsT ((TS b sub):xs) (Type a) | a == b     = sub
 subsT arr (Arrow a b) = Arrow (subsT arr a) (subsT arr b)
 
 algU' :: Type -> Type -> Substitution
-algU' a@(Type x) b@(Type _) | a == b = []
-                            | otherwise = [TS x b]
-algU' a@(Type x) b   | x `elem` freeTypes b = error "error"
-                    | otherwise            = [TS x b] 
-algU' a b@(Type _) = algU' b a
-algU' (Arrow a b) (Arrow c d) = algU' (subsT (algU' b d) a) (subsT (algU' b d) c) `composeSubs` algU' b d
+algU' a@(Type x) b@(Type _) | a == b                = []
+                            | otherwise             = [TS x b]
+algU' (Type x) b            | x `elem` freeTypes b  = error "error"
+                            | otherwise             = [TS x b] 
+algU' a b@(Type _)                                  = algU' b a
+algU' (Arrow a b) (Arrow c d)                       = algU' (subsT f a) (subsT f c) `composeSubs` f
+                                                            where f = algU' b d
 
 algU :: [(Type, Type)] -> Substitution
 algU [] = []
 algU ((Arrow a b, Arrow c d):xs) = algU ((a, c) : (b, d) : xs)
-algU ((a, b):xs) = ab ++ algU (map (\(x, y) -> ((subsT ab x), (subsT ab y))) xs) 
-                    where ab = algU' a b
+algU ((a, b):xs) =  ab `composeSubs` algU (map f xs) 
+                    where   ab = algU' a b
+                            f = \(x, y) -> ((subsT ab x), (subsT ab y))
 
 algE :: Context -> Term -> Type -> [(Type, Type)]
 algE g x a = algE' g x a $ freeTypes a
 
 algE' :: Context -> Term -> Type -> [Var] -> [(Type, Type)]
-algE' g (Var x) a _ = let x' = lookupContext x g
-                        in case x' of
+algE' g (Var x) a _ =   case lookupContext x g of
                             Just f -> (a, f):[]
                             Nothing -> []
-algE' g (App a b) t arr =   let fr = fresh $ arr
+algE' g (App a b) t arr =   let fr = freshType $ arr
                             in algE' g a (Arrow (Type fr) t) (fr:arr) ++ algE' g b (Type fr) (fr:arr)
-algE' g@(Context ctx) (Lam a b) t arr = let fo = fresh arr
-                                            ft = fresh $ fo:arr
-                                        in (t, Arrow (Type fo) (Type ft)) : algE (Context ((a, Type fo) : ctx)) b (Type ft)
+algE' g@(Context ctx) (Lam a b) t arr = let fo = freshType arr
+                                            ft = freshType $ fo:arr
+                                        in (t, Arrow (Type fo) (Type ft)) : algE' (Context $ (a, Type fo) : ctx) b (Type ft) (fo:ft:arr)
 
 algPP :: Term -> (Context, Type)
-algPP term = let func = algU . algE (Context gamma0) term $ sigma0
+algPP term = let func = reverse . algU . reverse . algE (Context gamma0) term $ sigma0
                  fr = free term
                  gamma0 = zip fr $ (map Type . take (length fr) $ arr)
                  arr = [V $ x : show i | i <- [1..] :: [Int], x <- ['A'..'Z']] 
--- src/Type.hs
+++ src/Type.hs
@@ -17,13 +17,13 @@ instance Show Term where
     show (Var (V n)) = n
     show App{..} = "(" ++ show alg ++ ") (" ++ show dat ++ ")"
     show Lam{..} = "\\" ++ show (Var var) ++ " -> " ++ show body
-    
+
 data Type =   Type Var
             | Arrow Type Type
             deriving (Read, Eq)
 instance Show Type where
     show (Type (V n)) = n
-    show (Arrow a b) = show a ++ " -> (" ++ show b ++ ")"
+    show (Arrow a b) = "(" ++ show a ++ ") -> " ++ show b
 
 newtype Context = Context [(Var, Type)]
             deriving (Show, Read, Eq)
