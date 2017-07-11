85c7c4bc5d817020c652bcfbe40da3bc2ea2ab80
code-creater <rino2000@yandex.ru>
2017-07-11 19:23:27 +0300

    Fix (Eq Term)

--- src/Reduce.hs
+++ src/Reduce.hs
@@ -5,10 +5,19 @@ import Data.Set
 import Type
 import Data.Text (pack)
 
+instance Eq Term where 
+    (Var x) == (Var y) = x == y
+    (App x y) == (App a b) = x == a && y == b
+    (Uni a) == (Uni b) = a == b
+    (Lam v1 t1 b1) == (Lam v2 t2 b2) = t1 == t2 && substitute b1 v1 (Var v2) == b2
+    (Fa v1 t1 b1) == (Fa v2 t2 b2) = t1 == t2 && substitute b1 v1 (Var v2) == b2
+    _ == _ = False
+
 fresh :: Set Var -> Var
 fresh conflicts = head . dropWhile (`member` conflicts) $ nameGen 
-		    where nameGen = V . pack <$> [a : freshName b | b <- [0..] :: [Int], a <- ['a'..'z']]
-			  freshName b = if b == 0 then "" else show b
+    where 
+        nameGen = V . pack <$> [a : freshName b | b <- [0..] :: [Int], a <- ['a'..'z']]
+        freshName b = if b == 0 then "" else show b
 
 free :: Term -> Set Var
 free Var{..} = singleton var
@@ -27,13 +36,13 @@ bound Uni{..} = empty
 alpha :: Term -> Set Var -> Term 
 alpha term conflicts = 
     case term of 
-	Var{}                   -> term
-	Uni{}                   -> term
-	App{..}                 -> App (alpha alg conflicts) (alpha dat conflicts)
-	Lam{..} |  isConflict   -> Lam var' tpe body'
-		|  otherwise    -> Lam var tpe (alpha body conflicts)
-	Fa {..} |  isConflict   -> Fa var' tpe body'
-		|  otherwise    -> Fa var tpe (alpha body conflicts)
+        Var{}                   -> term
+        Uni{}                   -> term
+        App{..}                 -> App (alpha alg conflicts) (alpha dat conflicts)
+        Lam{..} |  isConflict   -> Lam var' tpe body'
+                |  otherwise    -> Lam var tpe (alpha body conflicts)
+        Fa {..} |  isConflict   -> Fa var' tpe body'
+                |  otherwise    -> Fa var tpe (alpha body conflicts)
 	where 
 	    isConflict = var term `member` conflicts
 	    var'       = fresh $ conflicts `union` free (body term) `union` free (tpe term)
@@ -47,13 +56,13 @@ substitute term name newTerm =
 		| otherwise                 ->  Var var 
 	App{..}                             ->  App (subsNewTerm alg) (subsNewTerm dat)
 	Lam{var = var'}     | var' == name  ->  term
-			    | otherwise     ->  let Lam{..} = term' 
-						in  Lam var (subsNewTerm tpe) (subsNewTerm body)
+			            | otherwise     ->  let Lam{..} = term' 
+						                    in  Lam var (subsNewTerm tpe) (subsNewTerm body)
 	Fa {var = var'}     | var' == name  ->  term
-			    | otherwise     ->  let Fa {..} = term' 
-						in  Fa  var (subsNewTerm tpe) (subsNewTerm body)
+			            | otherwise     ->  let Fa {..} = term' 
+						                    in  Fa  var (subsNewTerm tpe) (subsNewTerm body)
     where 
-	term' = alpha term $ free newTerm 
+	term'               = alpha term $ free newTerm 
 	subsNewTerm oldTerm = substitute oldTerm name newTerm
 
 beta :: Term -> Term
@@ -64,25 +73,26 @@ beta term =
         (App Lam{..} dat )  -> substitute body var dat
         App{..}             ->  let alg' = beta alg 
                                     dat' = beta dat 
-                                in  if alg' /= alg 
-                                    then App alg' dat
-                                    else App alg dat'
+                                in  if alg' == alg 
+                                    then App alg dat'
+                                    else App alg' dat
         Lam{..}             -> mkBeta
         Fa {..}             -> mkBeta
     where
         mkBeta =    let tpe'    = beta $ tpe term
                         body'   = beta $ body term
-                    in  if tpe term == tpe' 
+                    in  if  tpe term == tpe' 
                         then term {tpe  = tpe'}
                         else term {body = body'}
 eta :: Term -> Term
 eta term = case term of
-    Uni{} -> term
-    Var{} -> term
+    Uni{}   -> term
+    Var{}   -> term
     App{..} -> App (eta alg) (eta dat)
-    Lam{..} ->  case body of 
-                    App alg Var{var = v} | v == var && v `notMember` free alg   -> eta alg
-                    _                                                           -> Lam var (eta tpe) (eta body)
+    Lam{..} ->  
+        case body of 
+            App alg Var{var = v} | v == var && v `notMember` free alg   -> eta alg
+            _                                                           -> Lam var (eta tpe) (eta body)
     Fa {..} -> Fa var (eta tpe) (eta body)     
 
 reduce :: Term -> Term 
--- src/Type.hs
+++ src/Type.hs
@@ -28,14 +28,6 @@ data Term   = Var
             { uni :: Uni }
             deriving (Show)
 
-instance Eq Term where 
-    (Var x) == (Var y) = x == y
-    (App x y) == (App a b) = x == a && y == b
-    (Uni a) == (Uni b) = a == b
-    (Lam v1 t1 b1) == (Lam v2 t2 b2) = t1 == t2 && substitute b1 v1 (Var v2) == b2
-    (Fa v1 t1 b1) == (Fa v2 t2 b2) = t1 == t2 && substitute b1 v1 (Var v2) == b2
-    _ == _ = False
-
 axiom :: Uni -> Uni 
 axiom Star = Box 1
 axiom (Box i) = Box $ i + 1
