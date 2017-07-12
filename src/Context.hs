733729df8047e72abd4c3b017170b7d1052a1a73
code-creater <rino2000@yandex.ru>
2017-07-12 11:50:01 +0300

    Error.hs Context.hs

---
+++ src/Context.hs
@@ -0,0 +1,17 @@
+module Context where
+
+import           Type
+import           Prelude            hiding (lookup)
+import qualified Prelude            as P (lookup)
+
+newtype Context = Context 
+                { getCtx :: [(Var, Term)] }
+
+lookup :: Var -> Context -> Maybe Term 
+lookup v (Context arr) = P.lookup v arr
+
+empty :: Context
+empty = Context []
+
+insert :: Var -> Term -> Context -> Context
+insert v t = Context . ((v, t):) . getCtx
