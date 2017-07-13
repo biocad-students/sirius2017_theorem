93c08fa9babb072b7fbf0c178a45af1c14a07ba3
vervolf2017 <allexx.03@mail.ru>
2017-07-13 15:52:56 +0400

    text

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,23 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Options.Generic
+import System.Console.Haskeline
+
+
+
 main :: IO ()
-main = do
-    putStrLn ""
-    return () 
+main = runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "> "
+           case minput of
+               Nothing -> return ()
+               Just "off" -> return ()
+               Just "exit" -> return ()
+               Just input -> do outputStrLn $ input ++ "!"
+                                loop
 
