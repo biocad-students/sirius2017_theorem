2c7e7825a04b029afa1b0d3245be2074ea2e5875
AlexHurt <alexey23081985@gmail.com>
2017-07-14 15:58:26 +0300

    Added parser for recod type

--- src/LawerHL/ParserHL.hs
+++ src/LawerHL/ParserHL.hs
@@ -21,18 +21,27 @@ parserVarTermPair :: Parser (Var, Term)
 parserVarTermPair = do pair <- (skipMany spaceChar) *> between (char '(') (char ')') parserVarTermPairMeta <* (skipMany spaceChar)
                        return pair
 
-parseIndParams :: Parser [(Var, Term)]
-parseIndParams = do items <- some parserVarTermPair
-                    return items
+parseParams :: Parser [(Var, Term)]
+parseParams = do items <- some parserVarTermPair
+                 return items
 
 parseInductive :: Parser Inductive
 parseInductive = do (V name) <- skipMany spaceChar *> string "inductive" *> skipSome spaceChar *> parserMetaVar <* skipMany spaceChar
-                    params <- parseIndParams
-                    conss <- string "=" *> parseIndParams
+                    params <- parseParams
+                    conss <- string "=" *> parseParams
                     return $ Inductive name (Context params) (Context conss)
+
+parseRecord :: Parser Record
+parseRecord = do (V name) <- skipMany spaceChar *> string "record" *> skipSome spaceChar *> parserMetaVar <* skipMany spaceChar
+                 params <- parseParams
+                 conss <- string "=" *> parseParams
+                 return $ Record name (Context params) (Context conss)
 
 parseMyInductive :: String -> IO ()
 parseMyInductive = parseTest parseInductive . pack
+
+parseMyRecord :: String -> IO ()
+parseMyRecord = parseTest parseRecord . pack
 
 parseMyPair :: String -> IO ()
 parseMyPair = parseTest parserVarTermPair . pack
--- src/LawerHL/Pretty.hs
+++ src/LawerHL/Pretty.hs
@@ -6,3 +6,6 @@ import LawerHL.Type
 
 instance Show Inductive where
     show Inductive{..} = "inductive " ++ show indName ++ " " ++ show indParams ++ " = " ++ show indConss
+	
+instance Show Record where
+    show Record{..} = "record " ++ show recName ++ " " ++ show recParams ++ " = " ++ show recConss
