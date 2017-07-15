7f74b2f48cac89f82132b20877b2b729b8e9787c
Korinin38 <artkash@bk.ru>
2017-07-15 13:56:16 +0400

    Modified testParserHL

--- test/TestParserHL.hs
+++ test/TestParserHL.hs
@@ -14,14 +14,14 @@ testsParserHL = describe "Parser HL test" $ do
 
 testAlg :: Spec
 testAlg = it "Parse Alg" $ do
-    parseTermM "data A b d= b | a b" `shouldBe` Just (Alg (Algebraic {algName = "A", algParams = ["b","d"], algConses = Context {getCtx = [(V "b",[]),(V "a",[TVar "b"])]}}))
+    parseTermM "data A b d = b | a b" `shouldBe` Just (Alg (Algebraic {algName = "A", algParams = ["b","d"], algConses = Context {getCtx = [(V "b",[]),(V "a",[TVar "b"])]}}))
     parseTermM "data A b d c x = b c x | a (c x) b" `shouldBe` Just (Alg (Algebraic {algName = "A", algParams = ["b","d","c","x"], algConses = Context {getCtx = [(V "b",[TVar "c",TVar "x"]),(V "a",[TApp (TVar "c") (TVar "x"),TVar "b"])]}}))
     parseTermM "data A b d c x = b c x | a (c x t) b" `shouldBe` Just (Alg (Algebraic {algName = "A", algParams = ["b","d","c","x"], algConses = Context {getCtx = [(V "b",[TVar "c",TVar "x"]),(V "a",[TApp (TApp (TVar "c") (TVar "x")) (TVar "t"),TVar "b"])]}}))
 
 testInd :: Spec
 testInd = it "Parse Ind" $ do
-    parseTermM "" `shouldBe` Nothing
+    parseTermM "inductive A (a:x)(b:*) = (A1:x->y->x)(B1:*->*)" `shouldBe` Just (Ind(Inductive{indName = "A", indParams = ["a""b"], indConses = Context {getCtx = [(V "A1",[TVar "x->y->x"]),(V "B1",[TVar "*->*"])]}}))
 
 testRec :: Spec
 testRec = it "Parse Rec" $ do
-    parseTermM "" `shouldBe` Nothing
+    parseTermM "record A (a:x)(b:*) = (A1:x->y->x)(B1:*->*)" `shouldBe` Just (Rec(Record{recName = "A", recParams = ["a""b"], recConses = Context {getCtx = [(V "A1",[TVar "x->y->x"]),(V "B1",[TVar "*->*"])]}}))
