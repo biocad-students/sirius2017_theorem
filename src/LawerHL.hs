bd575ffc110e54a7435b1426484f28988c640dbc
Korinin38 <artkash@bk.ru>
2017-07-16 17:54:44 +0400

    Remade TestParserHL

--- src/LawerHL.hs
+++ src/LawerHL.hs
@@ -1,7 +1,8 @@
 module LawerHL(
-    constructionToTerm,
+    parserConst, constructionToTerm, parserAlgebraic, parserInductive,
     Record(..), Inductive(..), TypeApp(..), Algebraic(..), Construction(..)
 ) where
 
+import LawerHL.Parser (parserConst, parserAlgebraic, parserInductive)
 import LawerHL.Encoding (constructionToTerm)
 import LawerHL.Type (Record(..), Inductive(..), TypeApp(..), Algebraic(..), Construction(..))
--- test/TestParserHL.hs
+++ test/TestParserHL.hs
@@ -11,30 +11,36 @@ main = hspec testsParserHL
 
 testsParserHL :: SpecWith ()
 testsParserHL = describe "Parser HL test" $ do  
-    testAlg
+    testAlg 
 
 parseConstruction :: String -> Maybe Construction
 parseConstruction = parseMaybe parserConst . pack
 
+parserAlgebraicM :: String -> Maybe Algebraic
+parserAlgebraicM = parseMaybe parserAlgebraic . pack
+
+parserInductiveM :: String -> Maybe Inductive
+parserInductiveM = parseMaybe parserInductive . pack
+
 testAlg :: Spec
 testAlg = it "Parse Alg" $ do
-    parseConstruction "data A" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [], algConses = Context []}))
-    parseConstruction "data A = a | b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [], algConses = Context {getCtx = [(V $ pack "a",[]),(V $ pack "b",[])]}}))
-    parseConstruction "data A a b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [pack "a", pack "b"], algConses = Context []}))
-    parseConstruction "data A b d = b | a b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [pack "b", pack "d"], algConses = Context {getCtx = [(V $ pack "b",[]),(V $ pack "a",[TVar $ pack "b"])]}}))
-    parseConstruction "data A b d c x = b c x | a (c x) b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [pack "b", pack "d", pack "c", pack "x"], algConses = Context {getCtx = [(V $ pack "b",[TVar $ pack "c",TVar $ pack "x"]),(V $ pack "a",[TApp (TVar $ pack "c") (TVar $ pack "x"),TVar $ pack "b"])]}}))
-    parseConstruction "data A b d c x = b c x | a (c x t) b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [ pack "b", pack "d", pack "c", pack "x"], algConses = Context {getCtx = [(V $ pack "b",[TVar $ pack "c",TVar $ pack "x"]),(V $ pack "a",[TApp (TApp (TVar $ pack "c") (TVar $ pack "x")) (TVar $ pack "t"),TVar $ pack "b"])]}}))
-    parseConstruction "data Data Data = a | b" `shouldBe`Just (Alg (Algebraic {algName = pack "Data", algParams = [pack "Data"], algConses = Context {getCtx = [(V $ pack "a",[]),(V $ pack "b",[])]}}))
-    parseConstruction "data A = a |" `shouldBe` Nothing
-    parseConstruction "data A =" `shouldBe` Nothing
-    parseConstruction "data = a b z" `shouldBe` Nothing
-    parseConstruction "data 1" `shouldBe` Nothing
+    parserAlgebraicM "data A" `shouldBe` Just (Algebraic {algName = pack "A", algParams = [], algConses = Context []})
+    parserAlgebraicM "data A = a | b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [], algConses = Context {getCtx = [(V $ pack "a",[]),(V $ pack "b",[])]}}))
+    parserAlgebraicM "data A a b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [pack "a", pack "b"], algConses = Context []}))
+    parserAlgebraicM "data A b d = b | a b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [pack "b", pack "d"], algConses = Context {getCtx = [(V $ pack "b",[]),(V $ pack "a",[TVar $ pack "b"])]}}))
+    parserAlgebraicM "data A b d c x = b c x | a (c x) b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [pack "b", pack "d", pack "c", pack "x"], algConses = Context {getCtx = [(V $ pack "b",[TVar $ pack "c",TVar $ pack "x"]),(V $ pack "a",[TApp (TVar $ pack "c") (TVar $ pack "x"),TVar $ pack "b"])]}}))
+    parserAlgebraicM "data A b d c x = b c x | a (c x t) b" `shouldBe` Just (Alg (Algebraic {algName = pack "A", algParams = [ pack "b", pack "d", pack "c", pack "x"], algConses = Context {getCtx = [(V $ pack "b",[TVar $ pack "c",TVar $ pack "x"]),(V $ pack "a",[TApp (TApp (TVar $ pack "c") (TVar $ pack "x")) (TVar $ pack "t"),TVar $ pack "b"])]}}))
+    parserAlgebraicM "data Data Data = a | b" `shouldBe`Just (Alg (Algebraic {algName = pack "Data", algParams = [pack "Data"], algConses = Context {getCtx = [(V $ pack "a",[]),(V $ pack "b",[])]}}))
+    parserAlgebraicM "data A = a |" `shouldBe` Nothing
+    parserAlgebraicM "data A =" `shouldBe` Nothing
+    parserAlgebraicM "data = a b z" `shouldBe` Nothing
+    parserAlgebraicM "data 1" `shouldBe` Nothing
 
 
---testInd :: Spec
---testInd = it "Parse Ind" $ do
---    parseConstruction "inductive A (a:x)(b:*) = (A1:x->y->x)(B1:*->*)" `shouldBe` Just (Ind(Inductive{indName = "A", indParams = ["a""b"], indConses = Context {getCtx = [(V "A1",[TVar "x->y->x"]),(V "B1",[TVar "*->*"])]}}))
+testInd :: Spec
+testInd = it "Parse Ind" $ do
+    parserInductiveM "inductive A (a:x)(b:*) = (A1:x)(B1:[])" `shouldBe` Just (Ind(Inductive{indName = pack "A", indParams = Context [(V . pack $ "a", Var $ V $ pack "x"), (V $ pack $ "b", Uni Star)], indConses = Context [(V $ pack $ "A1", Var $ V $ pack "x"),(V $ pack "B1",(Uni $ Star))]}))
 
---testRec :: Spec
---testRec = it "Parse Rec" $ do
---    parseConstruction "record A (a:x)(b:*) = (A1:x->y->x)(B1:*->*)" `shouldBe` Just (Rec(Record{recName = "A", recParams = ["a""b"], recConses = Context {getCtx = [(V "A1",[TVar "x->y->x"]),(V "B1",[TVar "*->*"])]}}))
+-- testRec :: Spec
+-- testRec = it "Parse Rec" $ do
+    -- parseConstruction "record A (a:x)(b:*) = (A1:x)(B1:[])" `shouldBe` Just (Rec(Record{recName = pack "A", recParams = Context [(V . pack $ "a", Var $ V $ pack "x"), (V $ pack $ "b", Uni Star)], recConses = Context [(V $ pack $ "A1", Var $ V $ pack "x"),(V $ pack "B1",(Uni $ Star))]}))
